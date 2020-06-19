daa9e992116ab8c14a8f41193debddb24e486d85
unknown <matarhar@gmail.com>
2020-06-19 03:32:42 +0200

    hello

---
+++ Cw3/Cw3/Controllers/StudentsController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cw3.DAL;
+using Cw3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cw3.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        private readonly IDbService _dbService;
+        public StudentsController(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+
+        [HttpGet]
+        public IActionResult GetStudents(string orderBy)
+        {
+            return Ok(_dbService.GetStudents());
+        } 
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            //... add to database 
+            //... generating index number 
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent(int id)
+        {
+            return Ok("Ąktualizacja dokończona");
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult PutStudent(int id)
+        {
+            return Ok("Usuwanie ukończone");
+        }
+    }
+
+    
+
+}
---
+++ Cw3/Cw3/DAL/IDbService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cw3.Models;
+using Cw3.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cw3.DAL
+{
+    public interface IDbService
+    {
+        public IEnumerable<Student> GetStudents();
+    }
+}
