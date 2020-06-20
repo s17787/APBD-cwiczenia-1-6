72d14d0803a8258e27458abb3b3e388028fb5f69
unknown <matarhar@gmail.com>
2020-06-20 14:08:16 +0200

    Cw5

---
+++ Cw5/Cw4/DTOs/Requests/EnrollStudentRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cw4.DTOs.Requests
+{
+    public class EnrollStudentRequest
+    {
+        public string IndexNumber { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public string Studies { get; set; }
+    }
+}
---
+++ Cw5/Cw4/Services/IStudentDbService.cs
@@ -0,0 +1,14 @@
+﻿using Cw4.DTOs.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cw4.Services
+{
+    public interface IStudentDbService
+    {
+        void EnrollStudent(EnrollStudentRequest request);
+        void PromoteStudents(int semester, string studies);
+    }
+}
